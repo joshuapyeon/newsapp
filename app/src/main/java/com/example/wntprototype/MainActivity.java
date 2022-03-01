e87a2f462a098684931a6521c0c598dbcb848c77
choIsaac2020 <isaac.j.cho@my.wheaton.edu>
2022-03-01 15:17:55 -0600

    Edited some of the data for all of you to view for today's meeting.

--- app/src/main/java/com/example/wntprototype/MainActivity.java
+++ app/src/main/java/com/example/wntprototype/MainActivity.java
@@ -1,29 +1,90 @@
 package com.example.wntprototype;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.content.Intent;
+import android.widget.*;
+import java.util.*;
 import com.example.wntprototype.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
+    TextView filterResults;
+    ArrayList<Integer> arraylist = new ArrayList<Integer>();
+    boolean[] selectedTopics;
+    boolean sportsSelected = false;
+    boolean WESelected = false;
+    boolean COVIDSelected = false;
+    String[] topics = {"Sports", "World Events", "COVID"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        filterResults = findViewById(R.id.textView);
+        filterResults.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainActivity.this
+                );
+                builder.setTitle("Filter Results");
+                builder.setCancelable(false);
+                builder.setMultiChoiceItems(topics, selectedTopics, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                        if(b) {
+                            if(topics[i].equals("Sports")) {
+                                sportsSelected = true;
+                            }
+                            else if(topics[i].equals("World Events")) {
+                                WESelected = true;
+                            }
+                            else {
+                                COVIDSelected = true;
+                            }
+                        }
+                        else {
+                            if(topics[i].equals("Sports")) {
+                                sportsSelected = false;
+                            }
+                            else if(topics[i].equals("World Events")) {
+                                WESelected = false;
+                            }
+                            else {
+                                COVIDSelected = false;
+                            }
+                        }
+                    }
+                });
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(!COVIDSelected && !WESelected && !sportsSelected) {
+
+                        }
+                    }
+                });
+                builder.show();
+            }
+        });
+        selectedTopics = new boolean[topics.length];
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -33,5 +94,4 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
--- app/src/main/java/com/example/wntprototype/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/wntprototype/ui/home/HomeFragment.java
@@ -1,29 +1,37 @@
 package com.example.wntprototype.ui.home;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import com.example.wntprototype.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
