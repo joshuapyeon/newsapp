a8d53839690743ef06630f5abf98aa77fce56721
0xFFD800 <lukedgreene@gmail.com>
2022-03-10 14:57:34 -0600

    Clicking search bar now opens filter view

--- app/src/main/java/com/example/wntprototype/MainActivity.java
+++ app/src/main/java/com/example/wntprototype/MainActivity.java
@@ -1,41 +1,31 @@
 package com.example.wntprototype;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentContainerView;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 import android.content.Intent;
-import android.widget.*;
-import java.util.*;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.Toast;
+
 import com.example.wntprototype.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.example.wntprototype.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        this.findViewById(R.id.search_bar).setOnClickListener((u1) -> {
-            //Open filter menu
+        this.findViewById(R.id.search_bar).setOnClickListener((u1) -> MainActivity.this.findViewById(R.id.filter_view).setVisibility(View.VISIBLE));
+        this.findViewById(R.id.search_bar).setOnKeyListener((u1, u2, u3) -> {
+            MainActivity.this.findViewById(R.id.filter_view).setVisibility(View.VISIBLE);
+            return true;
         });
+        ((SearchView)this.findViewById(R.id.search_bar)).setOnSearchClickListener((u1) -> MainActivity.this.executeSearch(((SearchView)this.findViewById(R.id.search_bar)).getQuery().toString()));
+        this.findViewById(R.id.button_search).setOnClickListener((u1) -> MainActivity.this.executeSearch(((SearchView)this.findViewById(R.id.search_bar)).getQuery().toString()));
+        this.findViewById(R.id.button_close).setOnClickListener((u1) -> MainActivity.this.findViewById(R.id.filter_view).setVisibility(View.GONE));
 
         this.findViewById(R.id.visualize_button).setOnClickListener((u1) -> {
             //Open visualize dropdown
@@ -55,5 +45,11 @@ public class MainActivity extends AppCompatActivity {
             sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
             startActivity(Intent.createChooser(sharingIntent, "Share using"));
         });
+    }
+
+    protected void executeSearch(String keyword) {
+        //Requires Backend...
+        Toast.makeText(this.getBaseContext(), "Searching for headlines relating to " + keyword, Toast.LENGTH_LONG).show();
+        MainActivity.this.findViewById(R.id.filter_view).setVisibility(View.GONE);
     }
 }
