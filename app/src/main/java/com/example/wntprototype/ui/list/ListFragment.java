f6bbecbc523af74774266ac9a127070cc43eedae
choIsaac2020 <isaac.j.cho@my.wheaton.edu>
2022-03-24 21:25:15 -0500

    Implemented listView and made some adjustments to it for the potential to add URL's for the user to click. Cached Data does not work. Made an else case to use WebSearchWrapper instead.

--- app/src/main/java/com/example/wntprototype/ui/list/ListFragment.java
+++ app/src/main/java/com/example/wntprototype/ui/list/ListFragment.java
@@ -1,9 +1,11 @@
 package com.example.wntprototype.ui.list;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,12 +14,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.wntprototype.APIWrappers.APIData;
+import com.example.wntprototype.APIWrappers.APISearch;
+import com.example.wntprototype.APIWrappers.WebSearchAPI.WebSearchWrapper;
 import com.example.wntprototype.DataCache;
 import com.example.wntprototype.R;
 import com.example.wntprototype.databinding.FragmentListBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class ListFragment extends Fragment {
     DataCache cache = DataCache.getCache();
@@ -33,6 +39,7 @@ public class ListFragment extends Fragment {
         binding = FragmentListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         listFormat = root.findViewById(R.id.listFormat);
+        APISearch search = new APISearch();
 
         if (cache.hasData()) {
             searchList = DataCache.getCache().getData();
@@ -42,9 +49,39 @@ public class ListFragment extends Fragment {
             for (APIData api : searchList) {
                 apiToStringList.add("#" + numID++ + ": " + api.getToParse());
             }
-            arrayAdapter = new ArrayAdapter(root.getContext(), android.R.layout.simple_list_item_1, apiToStringList);
-            listFormat.setAdapter(arrayAdapter);
         }
+        else {
+            try {
+                searchList = new WebSearchWrapper().execute(search).get();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int numID = 0;
+            for (APIData api : searchList) {
+                apiToStringList.add("#" + numID++ + ": " + api.getToParse());
+            }
+        }
+        arrayAdapter = new ArrayAdapter(root.getContext(), android.R.layout.simple_list_item_1, apiToStringList);
+        listFormat.setAdapter(arrayAdapter);
+        listFormat.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Snackbar sb = Snackbar.make(root, apiToStringList.get(i), Snackbar.LENGTH_SHORT);
+                sb.setAction("Article URL", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(root.getContext());
+                        builder.setMessage("Here's your url!");
+                        AlertDialog urlLink = builder.create();
+                        urlLink.show();
+                    }
+                });
+                sb.show();
+            }
+        });
+
         return root;
     }
 
