ebf0a3ec6c8dfe71805f36e62ffc1fb866650ac2
seena-thomas <seena.thomas@my.wheaton.edu>
2022-04-07 18:47:23 -0500

    Added x-axis labels to label the bar chart on the x-axis above. The first label is labeling twice, which is still a problem, but it is showing labels.
    Signed-off-by: seena-thomas <seena.thomas@my.wheaton.edu>

--- app/src/main/java/com/example/wntprototype/ui/graph/GraphFragment.java
+++ app/src/main/java/com/example/wntprototype/ui/graph/GraphFragment.java
@@ -20,6 +20,7 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
@@ -29,27 +30,44 @@ import java.util.List;
 public class GraphFragment extends Fragment {
 
     /**
-     * Cache that hodls the cached data
+     * Cache that holds the cached data
      */
     final private DataCache cache = DataCache.getCache();
     /**
-     * The following instance variables are
-     * for creating the structure of the graph
+     * The label associated with the
+     * graph (listed below the graph
+     * when it is displayed)
      */
 
-    private int XMAX = 5;
     private final String graphLabel = "Results & Values";
+    /**
+     * A list that stores all of the phrases
+     * associated with each result of the search
+     */
+    ArrayList<String> words = new ArrayList<String>();
+
     /**
      * The bar chart to be displayed
      */
     BarChart barChart;
+
     /**
      * The data that is retrieved from the user's
      * search (relevant articles or phrases)
      */
     List<TrendingContent> data;
-    ArrayList<String> words = new ArrayList<String>();
+    /**
+     * A list that stores all of the values
+     * associated with each result of the search.
+     */
     ArrayList<Integer> values = new ArrayList<Integer>();
+    /**
+     * The maximum value for the range
+     * of the x-axis
+     */
+
+    private int XMAX = 5;
+
     /**
      * Creates the binding for the
      * graph fragment
@@ -72,11 +90,12 @@ public class GraphFragment extends Fragment {
         binding = FragmentGraphBinding.inflate(inflater, container, false);
         View root = binding.getRoot(); //sets the view by getting the root
 
-        barChart = root.findViewById(R.id.bargraph);
+        barChart = root.findViewById(R.id.bargraph); //attaches a bar chart template to this fragment
 
+        //check whether an actual search was made for displaying data
         if (cache.hasData()) {
-            setData();
-            setAppearance();
+            setData(); //create the data that is to be displayed by operating on the results from the search
+            setAppearance(); //sets the structure of the graph itself (x and y axis, etc.)
             BarData barData = makeBarData();
             barData.setValueTextSize(16f);
             barChart.setData(barData);
@@ -84,55 +103,91 @@ public class GraphFragment extends Fragment {
         return root;
     }
 
-
+    /**
+     * Sets up the structure of the graph
+     * including the x-axis, as well as the y-axis.
+     */
     private void setAppearance() {
-        barChart.getDescription().setEnabled(true);
-        barChart.getDescription().setText(graphLabel);
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.getValueFormatter();
-        //  @Override
-        //public String xFormatter(int val){
-        //   return words.get(val);
-        //  }
-        // });
+        barChart.getDescription().setEnabled(false); //the description below the graph (the label) is not made visible
+        barChart.getDescription().setText(graphLabel); //set the text for the description for the graph
+        XAxis xAxis = barChart.getXAxis(); //obtain the x-axis for this graph
+        //sets the x-axis labels for each of the bars in the bar chart
+        xAxis.setValueFormatter(new ValueFormatter() {
+            @Override
+            /**
+             * Formats each x-axis bar label
+             * only labeling by one word from each
+             * phrase, if more than one
+             * word exists.
+             */
+            public String getFormattedValue(float val) {
+                String store = words.get((int) val);
+                if (store.contains(" ")) {
+                    int space = store.indexOf(" ");
+                    return store.substring(0, space);
+                }
+                return store;
+            }
+        });
+        xAxis.setTextSize(12f);
 
-        YAxis yLeft = barChart.getAxisLeft();
-        yLeft.setGranularity(5f);
-        yLeft.setAxisMinimum(0);
 
-        YAxis yRight = barChart.getAxisRight();
-        yRight.setGranularity(5f);
-        yRight.setAxisMinimum(0);
+        YAxis yLeft = barChart.getAxisLeft(); //get the left-side of the y-axis
+        yLeft.setGranularity(5f); //every five units, a numeric label will be given
+        yLeft.setAxisMinimum(0); //minimum y-value is 0
+
+
+        YAxis yRight = barChart.getAxisRight(); //get the right-side of the y-axis
+        yRight.setGranularity(5f); //every five units, a numeric label will be given
+        yRight.setAxisMinimum(0); //minimum y-value is 0
 
     }
 
+    /**
+     * Obtains data from the cache,
+     * which is data that is relevant
+     * to the user's most recent search
+     */
     private void setData() {
         Toast.makeText(getContext(), "Display graph", Toast.LENGTH_LONG).show();
-        data = cache.getData();
-        for (TrendingContent myData : data) {
-            words.add(myData.getPhrase());
-            values.add(myData.getValue());
+        data = cache.getData(); //obtain search data from the cache
 
+        for (TrendingContent myData : data) {
+            words.add(myData.getPhrase()); //add phrases from the search results to an array list
+            //System.out.println(myData.getPhrase());
+            values.add(myData.getValue()); //add values corresponding to each phrase to an array list
         }
+        //shows only the top five results in the bar graph for more visibility;
+        //otherwise, if there are less than five results,
+        //the maximum amount of bars will be set to whatever
+        //numbers of values there are from that search
         if (values.size() < 5) {
             XMAX = values.size();
         }
 
     }
 
+    /**
+     * Creates the bar chart based on the
+     * data.
+     *
+     * @return barData The bar chart data to be displayed
+     */
     private BarData makeBarData() {
         ArrayList<BarEntry> mappings = new ArrayList<>();
+        //creates bar entries (x and y value to create a bar)
+        //x values are indicated by which bar this is (0th, 1st, second, etc.)
         for (int i = 0; i < XMAX; i++) {
             int phraseNum = i;
             int thisNum = values.get(i);
-            System.out.println(thisNum);
-            mappings.add(new BarEntry(phraseNum, thisNum));
+            // System.out.println(thisNum);
+            mappings.add(new BarEntry(phraseNum, thisNum)); //add this bar entry to the array list of bar entries
         }
-        BarDataSet set = new BarDataSet(mappings, graphLabel);
+        BarDataSet set = new BarDataSet(mappings, graphLabel); //contains the set of all the bar entries
         set.setColors(ColorTemplate.JOYFUL_COLORS);
-        ArrayList<IBarDataSet> myDataSets = new ArrayList<>();
-        myDataSets.add(set);
-        BarData currentData = new BarData(myDataSets);
+        ArrayList<IBarDataSet> myDataSets = new ArrayList<>(); //can contain bar entry sets, including more than one
+        myDataSets.add(set); //add the bar entry set to list of BarDataSets
+        BarData currentData = new BarData(myDataSets); //set the bar dtat to what is stored in myDataSets
         return currentData;
 
 
