606b7630f1e26ee5884e6baf969d24223ac1e70d
choIsaac2020 <isaac.j.cho@my.wheaton.edu>
2022-04-07 21:30:33 -0500

    The user can now view their article in an internet browser

--- app/src/main/java/com/example/wntprototype/ui/list/ListFragment.java
+++ app/src/main/java/com/example/wntprototype/ui/list/ListFragment.java
@@ -1,7 +1,11 @@
 package com.example.wntprototype.ui.list;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.wntprototype.APIWrappers.NewsData;
 import com.example.wntprototype.APIWrappers.TrendingContent;
 import com.example.wntprototype.DataCache;
 import com.example.wntprototype.R;
@@ -25,6 +30,7 @@ public class ListFragment extends Fragment {
     DataCache cache = DataCache.getCache();
     List<TrendingContent> searchList;
     List<String> apiToStringList = new ArrayList<>();
+    List<NewsData> newsDataList = new ArrayList<NewsData>();
     private FragmentListBinding binding;
     ListView listFormat;
     ArrayAdapter<String> arrayAdapter;
@@ -35,21 +41,43 @@ public class ListFragment extends Fragment {
         binding = FragmentListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         listFormat = root.findViewById(R.id.listFormat);
-
         if (cache.hasData()) {
             searchList = DataCache.getCache().getData();
-
             int numID = 1;
-            for (TrendingContent api : searchList) {
+            for (TrendingContent api : searchList)
                 apiToStringList.add("#" + numID++ + ": " + api.getPhrase());
-            }
             arrayAdapter = new ArrayAdapter<>(root.getContext(), android.R.layout.simple_list_item_1, apiToStringList);
             listFormat.setAdapter(arrayAdapter);
             listFormat.setOnItemClickListener((adapterView, view, i, l) -> {
+                Log.d("Number: ", i+"");
                 Snackbar sb = Snackbar.make(root, apiToStringList.get(i), Snackbar.LENGTH_SHORT);
-                sb.setAction("Article URL", view1 -> {
+                sb.setAction("OPEN ARTICLE", view1 -> {
+                    NewsData nd = null;
+                    if(searchList.get(i).getArticles() != null)
+                        nd = searchList.get(i).getArticles().get(0);
                     AlertDialog.Builder builder = new AlertDialog.Builder(root.getContext());
-                    builder.setMessage("Here's your url!");
+                    if(nd != null) {
+                        if (nd.hasUrl()) {
+                            String url = nd.url;
+                            builder.setMessage(url);
+                            builder.setPositiveButton("See your Article", new DialogInterface.OnClickListener() {
+                                @Override
+                                /*
+                                 * Opens the url if there is one to go to.
+                                 */
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Intent openBrowser = new Intent();
+                                    openBrowser.setAction(Intent.ACTION_VIEW);
+                                    openBrowser.addCategory(Intent.CATEGORY_BROWSABLE);
+                                    openBrowser.setData(Uri.parse(url));
+                                    startActivity(openBrowser);
+                                }
+                            });
+                        }
+                        else
+                            builder.setMessage("URL Unavailable");
+                    }
+                    else builder.setMessage("URL Unavailable");
                     AlertDialog urlLink = builder.create();
                     urlLink.show();
                 });
