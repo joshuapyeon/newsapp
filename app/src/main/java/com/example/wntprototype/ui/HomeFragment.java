08e55c1188b7a7f6cd7e8759648065b7625fbbf2
natanjara95 <natanwj@gmail.com>
2022-04-27 23:43:37 -0500

    Menu Screen
    Signed-off-by: natanjara95 <natanwj@gmail.com>

---
+++ app/src/main/java/com/example/wntprototype/ui/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.example.wntprototype.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wntprototype.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
